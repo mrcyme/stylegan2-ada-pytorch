c5089a3ecd70f1da6c228312e4bbe0f533b088ef
mrcyme <simeonmcl@gmail.com>
2021-06-07 17:11:17 +0200

    Update generate.py
    add filename

--- generate.py
+++ generate.py
@@ -318,6 +318,7 @@ def generate_images(
     truncation_psi: float,
     noise_mode: str,
     outdir: str,
+    filename: str,
     class_idx: Optional[int],
     projected_w: Optional[str],
     start: Optional[float],
@@ -426,7 +427,7 @@ def generate_images(
             z = torch.from_numpy(np.random.RandomState(seed).randn(1, G.z_dim)).to(device)
             img = G(z, label, truncation_psi=truncation_psi, noise_mode=noise_mode)
             img = (img.permute(0, 2, 3, 1) * 127.5 + 128).clamp(0, 255).to(torch.uint8)
-            PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/seed{seed:04d}.png')
+            PIL.Image.fromarray(img[0].cpu().numpy(), 'RGB').save(f'{outdir}/{filename}{seed:04d}.png')
 
     elif(process=='interpolation' or process=='interpolation-truncation'):
         # create path for frames
